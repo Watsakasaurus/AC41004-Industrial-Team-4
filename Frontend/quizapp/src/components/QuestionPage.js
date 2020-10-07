28c612c571cfca75de28b1b8fb2d41931fd2d32f
Callum Darling <callum.darling@outlook.com>
2020-10-07 14:43:25 +0100

    Very basic button set up

--- Frontend/quizapp/src/components/QuestionPage.js
+++ Frontend/quizapp/src/components/QuestionPage.js
@@ -1,10 +1,36 @@
 import React, {Component} from "react";
+import Button from 'react-bootstrap/Button';
+
+
+
+function onButtonClick(identifier){
+    console.log(identifier);
+}
+
+
+
+function makeButton(text, callback){
+    return(<Button
+        // style={}
+        onClick={() => onButtonClick(text)}>
+            {text}
+        </Button>);
+}
 
 class QuestionPage extends Component {
+
+
+
     render() {
+        const element = makeButton("1")
         return (
             <div>
-                <h1>Look Here mate</h1>
+                    
+                    {element}
+                    {makeButton("2")}
+                    {makeButton("3")}
+                    {makeButton("4")}
+                    {makeButton("5")}
             </div>
             
         );
