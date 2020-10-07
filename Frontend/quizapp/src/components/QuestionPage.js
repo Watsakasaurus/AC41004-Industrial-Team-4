23bcddc76616654a5a9d3de266c41bcb670605de
Callum Darling <callum.darling@outlook.com>
2020-10-07 15:22:11 +0100

    Basic mapping of answers onto button

--- Frontend/quizapp/src/components/QuestionPage.js
+++ Frontend/quizapp/src/components/QuestionPage.js
@@ -8,31 +8,34 @@ function onButtonClick(identifier){
 }
 
 
-
-function makeButton(text, callback){
+// function that simply creates and returns a button with text and onclick funciton
+function makeButton(text, onClick){
     return(<Button
         // style={}
-        onClick={() => onButtonClick(text)}>
+        onClick={() => onButtonClick(onClick)}>
             {text}
         </Button>);
 }
 
 class QuestionPage extends Component {
-
-
+    constructor(props) {
+        super(props);
+        var cars = ["Which of these is not a car","Saab", "Volvo", "BMW","Dog"];
+        const map1 = cars.map(x => makeButton(x,x));
+        this.state = {bog: "bog", questions: cars, buttonMap: map1};
+      }
 
     render() {
         const element = makeButton("1")
         return (
             <div>
-                    
-                    {element}
-                    {makeButton("2")}
-                    {makeButton("3")}
-                    {makeButton("4")}
-                    {makeButton("5")}
+                <h1>{this.state.questions[0]}</h1>
+                {this.state.buttonMap[1]}
+                {this.state.buttonMap[2]}
+                <br/>
+                {this.state.buttonMap[3]}
+                {this.state.buttonMap[4]}
             </div>
-            
         );
     }
 
