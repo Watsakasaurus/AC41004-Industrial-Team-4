c6ff57b6a57c9f53e2ae47de21f679deb75f8c4e
Andrew Hart <andrewhart21@outlook.com>
2020-10-07 15:50:28 +0100

    Renamed .js file
    Simplified name of component and edited content

---
+++ Frontend/quizapp/src/Components/Results.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import Spinners from 'react-bootstrap/Spinner';
+import Badge from 'react-bootstrap/Badge';
+
+import './App.cs';
+
