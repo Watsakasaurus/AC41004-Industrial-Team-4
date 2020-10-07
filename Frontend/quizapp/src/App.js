8fc6f15f467f52b877ba69a7dc0eb669c368f593
Callum Darling <callum.darling@outlook.com>
2020-10-07 12:05:58 +0100

    Created question page

--- Frontend/quizapp/src/App.js
+++ Frontend/quizapp/src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import QuestionPage from './components/QuestionPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <QuestionPage>
+      </QuestionPage>
+      {/* <p>Look here mate</p> */}
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ Frontend/quizapp/src/components/QuestionPage.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class QuestionPage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Look Here mate</h1>
+            </div>
+            
+        );
+    }
+
+}
+
+export default QuestionPage
