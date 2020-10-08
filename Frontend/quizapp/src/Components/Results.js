55bccbef9a6fd4ed8f31d5e244461d4c009d08c1
Andrew Hart <andrewhart21@outlook.com>
2020-10-08 16:27:57 +0100

    Refactored mock-up

--- Frontend/quizapp/src/Components/Results.js
+++ Frontend/quizapp/src/Components/Results.js
@@ -1,69 +1,106 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import Spinners from 'react-bootstrap/Spinner';
-import Badge from 'react-bootstrap/Badge';
+import Container from "react-bootstrap/Container";
 
-import './Results.css';
-import { Row } from "react-bootstrap";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import ProgressBar from "react-bootstrap/ProgressBar";
+
+import Badge from "react-bootstrap/Badge";
+
+import "./Results.css";
+
+var jumbo = document.getElementById("jumbotron")
 
 function Result(props) {
-    return <h1 className="header"> {props.result}!</h1>;
-  }
-  
-  function Points(props){
-    return <h2 className="header"> {props.points} Points! </h2>
-  }
-
-  function Score(props){
-    return <ProgressBar>
-  <ProgressBar animated variant="success" now={props.score} key={1}  />
-        </ProgressBar>
-  }
-  
-
-  function UserBadge(props){
-     return <Badge pill variant = "primary" > {props.name} </Badge>
-  }
-
-function Results() {
-  return(
-
-<Container className="p-3">
-
-
-    <Jumbotron >
-        <div>
-            <Result result="" />
-        </div>
-
-        <div>
-            <Points points="" />
-        </div>
-    </Jumbotron>
-
-
-    <div>
-    <UserBadge name/>
- 
-
-    <Score score= "" />
- 
-    </div>
-
-    <div>
-        <Badge pill variant="secondary" >Name 2 </Badge>{' '}
-   
-        <ProgressBar>
-        <ProgressBar animated variant="success" now={35} key={1} />
-        </ProgressBar>
-    </div>
-  
-  </Container>
-);
-
+  return <h1 className="header"> {props.result}!</h1>;
 }
-export default Results;
+
+function Points(props) {
+  return <h2 className="header"> {props.points} Points! </h2>;
+}
+
+function Score(props) {
+  return (
+    <ProgressBar>
+      <ProgressBar animated variant="success" now={props.score} key={1} />
+    </ProgressBar>
+  );
+}
+function UserBadge(props) {
+  return (
+    <Badge pill variant="primary">
+      {"hh "}
+      {props.name}{"hh "}
+    </Badge>
+  );
+}
+
+class ResultsPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      correct: "Correct",
+      incorrect: "Incorrect",
+      colour: "green"
+    };
+    
+  }
+
+  setclr = () => {
+    if (Result === this.state.correct) {
+      this.setState({ color: "green" });
+      jumbo.style.backgroundColor = this.state.color;
+  }
+    else;
+
+  };
+  render() {
+    return (
+      <Container className="p-3">
+        <Jumbotron>
+          <div>
+            <Result result={this.state.correct} />
+          </div>
+
+          <div>
+            <Points points="100" />
+          </div>
+        </Jumbotron>
+
+        <div>
+          <UserBadge name />
+
+          <Score score="10" />
+        </div>
+
+        <div>
+          <Badge pill variant="secondary">
+            Name 2{" "}
+          </Badge>{" "}
+          <ProgressBar>
+            <ProgressBar animated variant="success" now={80} key={1} />
+          </ProgressBar>
+        </div>
+
+        <div>
+          <Badge pill variant="secondary">
+            Name 3{" "}
+          </Badge>{" "}
+          <ProgressBar>
+            <ProgressBar animated variant="success" now={80} key={1} />
+          </ProgressBar>
+        </div>
+
+        <div>
+          <Badge pill variant="secondary">
+            Name 4{" "}
+          </Badge>{" "}
+          <ProgressBar>
+            <ProgressBar animated variant="success" now={80} key={1} />
+          </ProgressBar>
+        </div>
+      </Container>
+    );
+  }
+}
+export default ResultsPage;
