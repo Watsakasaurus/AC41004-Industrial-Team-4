dac01f621972a30f67459ffd4d6b670cce198bbd
Alfie Hippisley <alfiehippisley10@gmail.com>
2020-10-08 16:26:35 +0100

    Nick Nameform Works (No Validation)

--- Frontend/quizapp/src/App.js
+++ Frontend/quizapp/src/App.js
@@ -1,31 +1,52 @@
 import React, { Component } from 'react';
 import Menu from './components/Menu';
 import Splash from './components/Splash';
+import EnterNickname from './components/EnterNickname';
 
-class App extends Component {   
+class App extends Component {
 
   constructor(props) {
     super(props);
-    
+
     this.state = {
+      nickname: "",
       inApp: false
     }
-    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
-    console.log();
-    this.setState({inApp: true});
+  // setNickname - Function Purpose : 
+  // Passed down as a prop to the EnterNickname component to allow that component to pass the nickname up.
+  // Sets the nickname into app.js state
+                      
+  setNickname(newNickname) {
+    console.log(newNickname);
+    this.setState({nickname: newNickname})
+  }
+
+  onClick(){
+    this.setState({inApp: true})
   }
 
   render() {
-
     return (
       <div>
-        {this.state.inApp ? 
-        <Menu playerNickname = "Alfie" onClick={this.handleClick}/>
-        : 
-        <Splash onClick={this.handleClick} />}
+        {!this.state.inApp ?
+          <Splash onClick={this.onClick.bind(this)} />
+          :
+          <div></div>}
+        
+        {this.state.inApp && !this.state.nickname ?
+          <EnterNickname changeValue={this.setNickname.bind(this)}/>
+          :
+          <div></div>}
+
+        {this.state.nickname ?
+          <Menu playerNickname ={this.state.nickname} onClick={this.onClick.bind(this)} />
+          :
+          <div></div>}
+
+
+        
       </div>
     )
 
---
+++ Frontend/quizapp/src/components/EnterNickname.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form'
+
+class EnterNickname extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        alert('A name was submitted: ' + this.state.value);
+        this.props.changeValue(this.state.value);
+    }
+
+    render() {
+
+        return (
+            <div className="Splash">
+
+                <Container className="Menu-container">
+                    <h1 className="Generic-center-title">Welcome!</h1>
+                </Container>
+
+                <Container className="Menu-container">
+                    <Row className="justify-content-center">
+                        <Col xl={8} xs={10}>
+                            <Row className="justify-content-center">
+                                <h4>Please choose a Nickname</h4>
+                            </Row>
+                            <Row className="justify-content-center">
+                                <form id="nickname-form" onSubmit={this.handleSubmit}>
+                                    <Form.Control size="lg" type="text" placeholder="Enter Nickname" className="Nickname-inputbox" value={this.state.value} onChange={this.handleChange} />
+                                </form>
+                            </Row>
+                            <Row className="justify-content-center">
+                                <p className="Generic-center">Please do not choose a Nickname that could reveal personal infomation about yourself.</p>
+                                <p>eg AnnoyingSeagull69</p>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Container>
+
+                <Container className="Menu-container">
+                    <Row className="justify-content-center">
+                        <Button className="Menu-green-circles" form="nickname-form" type="submit" variant="success">
+                            <div className="Menu-button-text">
+                                Next
+                        </div>
+                        </Button>
+                    </Row>
+                </Container>
+
+            </div>
+        );
+
+    }
+}
+
+export default EnterNickname;
