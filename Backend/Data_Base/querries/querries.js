525583ea5679eb941703c36307ebd1f01e24c9b1
Aylin <gamena750@abv.bg>
2020-10-09 15:07:20 +0100

    cleared up a bit
    left all the essential stuff inside

--- Backend/Data_Base/querries/querries.js
+++ Backend/Data_Base/querries/querries.js
@@ -1,21 +1,31 @@
 const { Pool, Client } = require("pg")
 const dotenv = require("dotenv")
+const pool = new Pool()
 dotenv.config();
 
-const connect_link = 'posgressql://aylin:aylin@localhost:5432/Uni_db'
+var num = 1; 
+const cat = "animals"; 
+//const connect_link = 'posgressql://aylin:aylin@161.35.170.36:5432/Uni_db'
 
 
 const client = new Client();
 client.connect().then(() => {
 
 
-    client.query('SELECT * FROM quiz', (err, res) => {
+    client.query('SELECT * FROM quiz limit ' + num, (err, res) => {
         console.log(err, res)
         client.end()
     })
 }).catch(err => {
     console.error(err);
+    
 });
+
+
+
+
+
+
 
 
 
