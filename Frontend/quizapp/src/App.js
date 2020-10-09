c6f2eb0de5a58bb8a46826302e9e4f4fa7d611cd
Alfie Hippisley <alfiehippisley10@gmail.com>
2020-10-09 16:14:02 +0100

    Spash Screen, Nickname Selection & Home Screen (#2)
    * Home Page HTML & CSS Complete
    
    I have completed the HTML & CSS for the home page.
    
    * Moved CSS, Started Splash Screen
    
    * Splash Screen Added
    
    I have added the application splash screen when it is first started. I have also linked this to the home page,
    
    * Nick Nameform Works (No Validation)
    
    * Removed Useless CSS File
    
    No longer needed the Menu.css file, also added some comments.
    
    * Basic Input Validation For Nickname
    
    I have added some basic input validation for the nickname form.

--- Frontend/quizapp/src/App.js
+++ Frontend/quizapp/src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Menu from './components/Menu';
+import Splash from './components/Splash';
+import EnterNickname from './components/EnterNickname';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      nickname: "",
+      inApp: false
+    }
+  }
+
+  // setNickname - Function Purpose : 
+  // Passed down as a prop to the EnterNickname component to allow that component to pass the nickname up.
+  // Sets the nickname into app.js state
+                      
+  setNickname(newNickname) {
+    console.log(newNickname);
+    this.setState({nickname: newNickname})
+  }
+
+  // Using this for testing :)
+  onClick(){
+    this.setState({inApp: true})
+  }
+
+
+  // Yes the code below this is an utter mess, its for testing, dont panic ;)
+  render() {
+    return (
+      <div>
+        {!this.state.inApp ?
+          <Splash onClick={this.onClick.bind(this)} />
+          :
+          <div></div>}
+        
+        {this.state.inApp && !this.state.nickname ?
+          <EnterNickname changeValue={this.setNickname.bind(this)}/>
+          :
+          <div></div>}
+
+        {this.state.nickname ?
+
+          // When calling the menu, pass the player nick name as a prop
+          <Menu playerNickname ={this.state.nickname} onClick={this.onClick.bind(this)} />
+          :
+          <div></div>}
+
+
+        
+      </div>
+    )
+
+  };
 }
+
 
 export default App;
