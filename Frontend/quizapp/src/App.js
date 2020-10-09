b086adab72a192f4d35f88d8d13cf436faacc053
Callum Darling <callum.darling@outlook.com>
2020-10-09 17:02:37 +0100

    Started creating flow-through between newly merged components

--- Frontend/quizapp/src/App.js
+++ Frontend/quizapp/src/App.js
@@ -30,30 +30,22 @@ class App extends Component {
     this.setState({inApp: true})
   }
 
-
-  // Yes the code below this is an utter mess, its for testing, dont panic ;)
-  render() {
-    return (
-      <div>
-        {!this.state.inApp ?
-          <Splash onClick={this.onClick.bind(this)} />
-          :
-          <div></div>}
-        
-        {this.state.inApp && !this.state.nickname ?
+  returnComponent(compID){
+    switch(compID) {
+      case 1:
+        return(
+          <Splash onClick={this.onClick.bind(this)}/>
+        );
+        // code block
+        break;
+      case 2:
+        return(
           <EnterNickname changeValue={this.setNickname.bind(this)}/>
-          :
-          <div></div>}
-
-        {this.state.nickname ?
-
-          // When calling the menu, pass the player nick name as a prop
-          <Menu playerNickname ={this.state.nickname} onClick={this.onClick.bind(this)} />
-          :
-          <div></div>}
-
-
-       <QuestionPage questions={[["Which of thghese is not a car","Saab", "Volvo", "BMW","Dog"],
+        );
+        break;
+      case 3:
+        return(
+          <QuestionPage questions={[["Which of thghese is not a car","Saab", "Volvo", "BMW","Dog"],
                                 ["Which of theghse is not a car","Saab", "Volvo", "BMW2","Dog2"],
                                 ["Which of these is not a car","Saab", "Volvo", "BMW3","Dog3"],
                                 ["Which of thesghjghe is not a car","Saab", "Volvo", "BMW4","Dog4"],
@@ -71,14 +63,66 @@ class App extends Component {
                                 ["Which of these is not a lcar","Saab", "Volvo", "BMW4","sDog4"],
                                 ["Which of these is not a clar","Saab", "Volvo", "BMW4","Dog4fd"],
                                 ["Which of these is not a calr","Saab", "Volvo", "BMW4","Dogcx4"],]}
-      >
-      </QuestionPage>
-      
-      
-      <ResultsPage>
+        ></QuestionPage>
+        );
+        break;
+      case 4:
+        return(
+          <ResultsPage></ResultsPage>
+        );
+            // code block
+        break;
+      case 5:
+        return(
+          <Menu playerNickname ={this.state.nickname} onClick={this.onClick.bind(this)} />
+        );
+        break;
+      default:
+        return(
+          <h1>You probably shouldnt be seeing this </h1>
+        );
+    }
+    
+  }
+
+  
+
+  // Yes the code below this is an utter mess, its for testing, dont panic ;)
+  render() {
+
+
+// <div>
+//         {!this.state.inApp ?
+//           <Splash onClick={this.onClick.bind(this)} />
+//           :
+//           <div></div>}
         
-      </ResultsPage> 
-                                
+//         {this.state.inApp && !this.state.nickname ?
+//           <EnterNickname changeValue={this.setNickname.bind(this)}/>
+//           :
+//           <div></div>}
+
+//         {this.state.nickname ?
+
+//           // When calling the menu, pass the player nick name as a prop
+//           <Menu playerNickname ={this.state.nickname} onClick={this.onClick.bind(this)} />
+//           :
+//           <div></div>}
+
+
+    var compNo = -1;
+    if(!this.state.inApp){
+      compNo = 1;
+    }else if(this.state.inApp && !this.state.nickname){
+      compNo = 2;
+    }else if(this.state.nickname){
+      compNo = 5; 
+    }
+
+
+    return (
+      <div>
+        {this.returnComponent(compNo)}                        
       </div>
          
          
