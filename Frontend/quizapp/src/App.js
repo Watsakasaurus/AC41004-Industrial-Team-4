c13f6b1c5b8aa827bf21ff66a91e4af218bede0f
andyhart21 <37302743+andyhart21@users.noreply.github.com>
2020-10-09 16:27:43 +0100

    Update App.js

--- Frontend/quizapp/src/App.js
+++ Frontend/quizapp/src/App.js
@@ -6,6 +6,23 @@ import ResultsPage from './Components/Results';
 
 function App() {
   return (
+    function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     <div>
       <ResultsPage>
         
