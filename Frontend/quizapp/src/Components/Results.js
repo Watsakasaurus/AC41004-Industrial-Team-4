e1d7b7e61e3928c3beecdc7471d23061edb0fc22
Alfie Hippisley <alfiehippisley10@gmail.com>
2020-10-13 13:51:13 +0100

    Removed Random CSS File/Minor Style Tweaks For Results Comp
    I have changed a few stye issues and corrected some indentation that went a bit crazy.
    
    Style changes make the app easier to use for some users, eg color change/font.
    
    Changed name align on results to left, would be intrested to see what other people think the location of the names should be.

--- Frontend/quizapp/src/Components/Results.js
+++ Frontend/quizapp/src/Components/Results.js
@@ -7,16 +7,9 @@ import ProgressBar from "react-bootstrap/ProgressBar";
 
 import Badge from "react-bootstrap/Badge";
 
-import "./Results.css";
-
-var jumbo = document.getElementById("jumbotron")
-var question_right = null
-
 function Result(props) {
-
   //if (question_right ==  1)
-    return <h1 className="Ques_result"> {props.result}!</h1>;
- 
+  return <h1 className="Ques_result"> {props.result}!</h1>;
 }
 
 function Points(props) {
@@ -26,37 +19,34 @@ function Points(props) {
 function Score(props) {
   return (
     <ProgressBar>
-      <ProgressBar  variant={props.user_clr} now={props.score} label={`${props.score} points`} key={1} />
-      
+      <ProgressBar variant={props.user_clr} now={props.score} label={`${props.score} points`} key={1} />
     </ProgressBar>
   );
 }
+
 function UserBadge(props) {
   return (
-    <Badge className="user_badge" pill variant = {props.user_clr}>
+    <Badge className="user_badge Results-Larger-badge" variant={props.user_clr}>
       {" "}
       {props.name}{" "}
     </Badge>
   );
 }
 
-function TimeLeft(props){
-
-    return <h2 className="time_left">   Loading Next Question!</h2>;
- 
-           
-
+function TimeLeft(props) {
+  return <h2 className="time_left">   Loading Next Question!</h2>;
 }
 
 
-window.onload = function(){
+window.onload = function () {
   setInterval(count, 1000);
 };
 
 let counter = 0;
-function count(){
+
+function count() {
   counter++;
- 
+
   if (counter < 10)
     return <h1 className="counter"> 10 </h1>
   else if (counter == 10)
@@ -71,91 +61,94 @@ class ResultsPage extends Component {
       incorrect: "Incorrect",
       colour: "#28A745"
     };
-   // const { playerNickname } = props;
+    // const { playerNickname } = props;
 
   }
 
-  
   setclr = () => {
     if (Result === this.state.correct) {
       this.setState({ colour: "green" });
     }
     else if (Result === this.state.incorrect) {
-      this.setState({ colour: "red"});
-    
-  }
-      
+      this.setState({ colour: "red" });
+    }
   };
 
-   
-
- 
   render() {
 
 
     let players = ["Andrew", "Alfie", "Sophie", "Callum"];
-    let colours = ["primary", "success", "danger" , "warning"]
-    let plyr_score = ["100", "85" , "45" , "20"]
-     
+    let colours = ["primary", "success", "danger", "warning"]
+    let plyr_score = ["100", "85", "45", "20"]
+
 
     return (
       <Container className="p-3">
 
-        <div> 
-          <Jumbotron className="jumbotron" style={{backgroundColor: this.state.colour}}>
-            <div>
+        <div>
+          <Jumbotron className="jumbotron" style={{ backgroundColor: this.state.colour }}>
+            <Container className="Results-part1">
               <Result result={this.state.correct} />
-            </div>
+            </Container>
 
-            <div>
+            <Container className="Results-part1">
               <Points points="100" />
-            </div>
+            </Container>
           </Jumbotron>
         </div>
 
         <div>
-          <UserBadge name = {players[0]} user_clr = {colours[0]} />
-
-          <Score  score = {plyr_score[0]} user_clr = {colours[0]} />
+          <div className="Result-div">
+            <UserBadge name={players[0]} user_clr={colours[0]} />
+          </div>
+          <div className="Result-div2">
+            <Score score={plyr_score[0]} user_clr={colours[0]} />
+          </div>
         </div>
 
         <div>
-        <UserBadge name = {players[1]} user_clr = {colours[1]} />
-
-        <Score  score = {plyr_score[1]} user_clr = {colours[1]}/>   
+          <div className="Result-div">
+            <UserBadge name={players[1]} user_clr={colours[1]} />
+          </div>
+          <div className="Result-div2">
+            <Score score={plyr_score[1]} user_clr={colours[1]} />
+          </div>
         </div>
 
         <div>
-          <UserBadge name = {players[2]} user_clr = {colours[2]}/>
-
-          <Score  score = {plyr_score[2]} user_clr = {colours[2]}/>   
+          <div className="Result-div">
+            <UserBadge name={players[2]} user_clr={colours[2]} />
+          </div>
+          <div className="Result-div2">
+            <Score score={plyr_score[2]} user_clr={colours[2]} />
+          </div>
         </div>
 
         <div>
-        <UserBadge name = {players[3]} user_clr = {colours[3]}/>
-        <Score  score = {plyr_score[3]} user_clr = {colours[3]}/>   
+          <div className="Result-div">
+            <UserBadge name={players[3]} user_clr={colours[3]} />
+          </div>
+          <div className="Result-div2">
+            <Score score={plyr_score[3]} user_clr={colours[3]} />
+          </div>
         </div>
 
         <br>
-        
         </br>
 
         <div>
-        <Jumbotron className="jumbotron-2" style={{backgroundColor: this.state.colour}}>
-          <div> 
-            <count />
-            <Spinner animation="border" role="status">
+          <Container className="Results-loading">
+            <div>
+              <count />
+              <Spinner animation="border" role="status">
                 <span className="sr-only">Loading...</span>
-            </Spinner>
+              </Spinner>
             </div>
 
             <div>
-
-          <TimeLeft  />
-           </div>
-     
-          </Jumbotron>
-
+              <TimeLeft />
+            </div>
+          </Container>
         </div>
       </Container>
     );
