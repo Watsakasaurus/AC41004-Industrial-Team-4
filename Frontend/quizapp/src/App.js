3d2c3bb1fc4e65fc158d2db65b26d94fc5316ada
Alfie Hippisley <alfiehippisley10@gmail.com>
2020-10-15 11:25:32 +0100

    Quick Foundation Of Component

--- Frontend/quizapp/src/App.js
+++ Frontend/quizapp/src/App.js
@@ -4,7 +4,8 @@ import Splash from './components/Splash';
 import EnterNickname from './components/EnterNickname';
 import QuestionPage from './components/QuestionPage';
 import ResultsPage from './components/Results';
-import {RoomConfigure} from './components/RoomConfigure'   
+import RoomConfigure from './components/RoomConfigure';
+import EnterRoomNumber from './components/EnterRoomNumber';
 
 const testQuestions = [["This drink contains caffeine.","A Mineral water","B Orange juice","C Coffee","D Beer",3],
                       ["Finish the proverb:","Poets are born, ________.","A ...not made.","B ...but can also be made.","C ...but thats not for sure.","D ..., long live the poets!",1],
@@ -133,9 +134,11 @@ class App extends Component {
   // Render a component based on the state variable currentComp
   render() {
     return (
-      <div>
-        {this.returnComponent(this.state.currentComp)}                        
-      </div>
+      //<div>
+      //  {this.returnComponent(this.state.currentComp)}                        
+      //</div>
+
+      <EnterRoomNumber />
     )
   };
 }
---
+++ Frontend/quizapp/src/components/EnterRoomNumber.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form'
+
+class EnterRoomNumber extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    // This will update the component state each input
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    // When the button to submit the form is pressed, pass the form input as a prop up to parent via the passed down function
+    handleSubmit(event) {
+        event.preventDefault() // Prevent default prevents the page refreshing
+        this.props.changeValue(this.state.value);
+    }
+
+    render() {
+        return (
+            <div className="Menu">
+                <Container className="Menu-container">
+                    <h1 className="Generic-center-title">Welcome!</h1>
+                </Container>
+
+                <Container className="Menu-container">
+                    <Row className="justify-content-center">
+                        <Col xl={8} xs={10}>
+                            <Row className="justify-content-center">
+                                <h4>Please choose a Nickname</h4>
+                            </Row>
+                            <Row className="justify-content-center">
+                                <form id="nickname-form" onSubmit={this.handleSubmit}>
+                                    <Form.Control size="lg" type="text" placeholder="Enter Nickname" className="Nickname-inputbox" pattern="^\S+$" maxlength="15" value={this.state.value} onChange={this.handleChange} />
+                                </form>
+                            </Row>
+                            <Row className="justify-content-center">
+                                
+                                <p>No Spaces, 15 Characters Max</p>
+                                <p className="Disclaimer Generic-center">Please do not choose a Nickname that could reveal personal infomation about yourself.</p>
+                                <p>eg AnnoyingSeagull69</p>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Container>
+
+                <Container className="Menu-container">
+                    <Row className="justify-content-center">
+                        <Button className="Menu-green-circles" form="nickname-form" type="submit" variant="success">
+                            <div className="Menu-button-text">
+                                Next
+                        </div>
+                        </Button>
+                    </Row>
+                </Container>
+
+            </div>
+        );
+
+    }
+}
+
+export default EnterRoomNumber;
