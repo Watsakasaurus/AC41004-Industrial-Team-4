28ee6686b51bd455ff06ad8a850f30298278c81e
Alfie Hippisley <alfiehippisley10@gmail.com>
2020-10-15 11:03:57 +0100

    Added JS Functionality
    Needs players passed as prop but until then this will do for now :)

--- Frontend/quizapp/src/components/Lobby.js
+++ Frontend/quizapp/src/components/Lobby.js
@@ -1,76 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 
-// This code should recieve one prop, 'playerNickname' which is displayed in the hello message.
+class Lobby extends Component {
 
-function Lobby(props) {
+    constructor(props) {
+        super(props);
 
-    // Saves having to type 'this.props', instead just 'props'
-    const { playerNickname } = props;
+        // This obviously will need to be passed down from app.js in the real thing
+        this.state = {
+            players: ['Alfie', 'Callum', 'Sophie', 'Andrew', 'Peter', 'Arran', 'Nicole', 'Callum', 'Ross', 'Aylin'],
+        }
+    }
 
-    return (
-        <div className="Menu">
-            <Container className="Menu-container">
-                <Container>
-                    <h1 className="Menu-title">Waiting For Players</h1>
-                    <h4>Use the room code '1234' to join!</h4>
+    render() {
+        return (
+            <div className="Menu">
+                <Container className="Menu-container">
+                    <Container>
+                        <h1 className="Menu-title">In Room - Waiting For Players</h1>
+                        <h4>Use the room code '1234' to join!</h4>
+                    </Container>
+
+                    <Container fluid>
+                        <Row className="Menu-row">
+                            <Col>
+                                <h1>Current Player's</h1>
+                            </Col>
+                        </Row>
+                        <Row className="Menu-row Lobby-list">
+                            <Col>
+                                {this.state.players.map(item => (
+                                    <Container className="Lobby-player-container">
+                                        <h1 className="Lobby-player-text" key={item}>{item}</h1>
+                                    </Container>
+                                ))}
+                            </Col>
+                        </Row>
+                        <Row className="Menu-row">
+                            <Col>
+                                <Button className="Menu-button" block variant="danger">
+                                    <h1 className="Menu-cancel">
+                                        x
+                                    </h1>
+                                </Button>
+                            </Col>
+                            <Col>
+                                <Button className="Menu-button Menu-green-circles" block variant="success">
+                                    <h1 className="Splash-button">
+                                        Begin!
+                                    </h1>
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Container>
                 </Container>
-
-                <Container fluid>
-                    <Row className="Menu-row">
-                        <Col>
-                            <h1>Current Player's</h1>
-                        </Col>
-                    </Row>
-                    <Row className="Menu-row Lobby-list">
-                        <Col>
-                            <Container className="Lobby-player-container">
-                                <h1 className="Lobby-player-text">Alfie</h1>
-                            </Container>
-                            <Container className="Lobby-player-container">
-                                <h1 className="Lobby-player-text">Alfie</h1>
-                            </Container>
-                            <Container className="Lobby-player-container">
-                                <h1 className="Lobby-player-text">Alfie</h1>
-                            </Container>
-                            <Container className="Lobby-player-container">
-                                <h1 className="Lobby-player-text">Alfie</h1>
-                            </Container>
-                            <Container className="Lobby-player-container">
-                                <h1 className="Lobby-player-text">Alfie</h1>
-                            </Container>
-                            <Container className="Lobby-player-container">
-                                <h1 className="Lobby-player-text">Alfie</h1>
-                            </Container>
-                            <Container className="Lobby-player-container">
-                                <h1 className="Lobby-player-text">Alfie</h1>
-                            </Container>
-                        </Col>
-                    </Row>
-                    <Row className="Menu-row">
-            <Col>
-              <Button className="Menu-button" block variant="danger">
-                <h1 className="Menu-cancel">
-                  x
-                </h1>
-              </Button>
-            </Col>
-            <Col>
-              <Button className="Menu-button Menu-green-circles" block variant="success">
-                <h1 className="Splash-button">
-                  Begin!
-                </h1>
-              </Button>
-            </Col>
-          </Row>
-                </Container>
-            </Container>
-
-        </div>
-    );
+            </div>
+        );
+    }
 }
 
 export default Lobby;
